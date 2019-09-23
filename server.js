6ee0ddb2efe71bdf84611161af622704a51addef
Viviana Montiel <vivimontiel@gmail.com>
2019-09-23 12:35:43 +0200

    ajout d'un nouveau point d'entree

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/test', (req, res) => {
+  
+  if (req.body.msg == "ville") {
+    res.send('Nous sommes à Paris');
+  } else if (req.body.msg == "meteo") {
+    res.send('Il fait beau');
+  } else {
+      res.send('Try Again');
+  }
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
